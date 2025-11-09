9b29aba0825cac0b94084f5ea7bfcc69958dbaf1
Dittor67 <100069603+Dittor-Aud@users.noreply.github.com>
2025-11-09 18:14:18 +0700

    Update Discord link in RequestTag class

--- src/layers/requestTag.cpp
+++ src/layers/requestTag.cpp
@@ -43,7 +43,7 @@ bool RequestTag::setup(std::vector<std::variant<std::string, bool>> level) {
 }
 
 void RequestTag::discord(CCObject* sender) {
-    web::openLinkInBrowser("https://discord.gg/6GXYHf9WTB");
+    web::openLinkInBrowser("https://discord.gg/ZFF5Rgfb5B");
 }
 
 RequestTag* RequestTag::create(std::vector<std::variant<std::string, bool>> level) {
